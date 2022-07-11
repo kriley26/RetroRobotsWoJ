db03b05feff8643b04c571579042da439193913e
nchampagnejhu <107518478+nchampagnejhu@users.noreply.github.com>
2022-07-11 10:44:39 -0700

    Created test package

---
+++ src/retrorobotswoj/test/WoJTest.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package retrorobotswoj.test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author nick
+ */
+public class WoJTest {
+    @Test
+    void demoTestMethod() {
+        assertTrue(true);
+    }
+}
