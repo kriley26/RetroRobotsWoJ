3968d58ddb01a06ae607ddeb257da5691c098cb7
nchampagnejhu <107518478+nchampagnejhu@users.noreply.github.com>
2022-07-18 13:46:47 -0700

    Update GUITests.java

--- WheelOfJeopardy/gui/src/test/java/test/GUITests.java
+++ WheelOfJeopardy/gui/src/test/java/test/GUITests.java
@@ -6,10 +6,13 @@ package test;/*
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.retrorobots.playerGUI.QuestionPanel;
-import com.retrorobots.server.ServerApplication;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -41,23 +44,22 @@ public class GUITests {
     void testDatabaseConnection() {
         boolean databaseConnectionTest = false;
 
+        /**
+         * If database connection successful, set boolean databaseConnectionTest = true
+         */
         try {
             URL url = new URL("http://localhost:8080/question");
             HttpURLConnection con = (HttpURLConnection)url.openConnection();
             con.setRequestMethod("GET");
-            System.out.println("test");
 
             BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
             String inputLine;
             StringBuffer content = new StringBuffer();
             while ((inputLine = in.readLine()) != null) {
                 content.append(inputLine);
-                System.out.println(inputLine);
-                System.out.println(content);
             }
-            if (inputLine != null) { databaseConnectionTest = true; }
+            if (content != null) { databaseConnectionTest = true; }
             in.close();
-
             con.disconnect();
 
         } catch (MalformedURLException ex) {
@@ -67,6 +69,8 @@ public class GUITests {
         } catch (IOException ex) {
             Logger.getLogger(QuestionPanel.class.getName()).log(Level.SEVERE, null, ex);
         }
+
+        //databaseConnectionTest = true;
 
         assertTrue(databaseConnectionTest == true);
     }
